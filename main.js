af087983e537119f48bfd74c7397424a0099787a
bieltomas <gtomasvidal@cifpfbmoll.eu>
2023-02-22 21:03:16 +0100

    third commit

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ function imprimirCoches() {
     document.getElementById("pista").innerHTML += "<div class='flex'><img src='images/ferrari.gif' class='coche relative h-48' id='coche1' /></div>";
     document.getElementById("pista").innerHTML += "<div class='flex'><img src='images/mercedes.gif' class='coche relative h-48' id='coche1' /></div>";
     document.getElementById("pista").innerHTML += "<div class='flex'><img src='images/formulae.gif' class='coche relative h-48' id='coche1' /></div>";
+    document.getElementById("pista").innerHTML += "<div class='flex'><img src='images/mclaren.gif' class='coche relative h-16 mb-12' id='coche1' /></div>";
+
 }
 
 function carrera() {
@@ -19,21 +21,52 @@ function carrera() {
             console.log(coches[i].offsetLeft + random + "px");
         }
 
-        if (coches[0].offsetLeft >= document.getElementById("pista").offsetWidth - 50) {
-           alert("Ganó el Ferrari");
+        if (coches[0].offsetLeft >= document.getElementById("pista").offsetWidth - 50 && document.getElementById("opcion").value == "1") {
+            alert("Enhorabuena! Ganó el Ferrari");
             clearInterval(intervalo);
             break;
         }
-        else if (coches[1].offsetLeft >= document.getElementById("pista").offsetWidth - 50) {
-            alert("Ganó el Mercedes");
+        else if (coches[0].offsetLeft >= document.getElementById("pista").offsetWidth - 50 && document.getElementById("opcion").value != "1") {
+            alert("Mala suerte... Ganó el Ferrari");
             clearInterval(intervalo);
             break;
         }
-        else if (coches[2].offsetLeft >= document.getElementById("pista").offsetWidth - 50) {
-           alert("Ganó el Formula E");
+
+        if (coches[1].offsetLeft >= document.getElementById("pista").offsetWidth - 50 && document.getElementById("opcion").value == "2") {
+            alert("Enhorabuena! Ganó el Mercedes");
             clearInterval(intervalo);
             break;
         }
+        else if (coches[1].offsetLeft >= document.getElementById("pista").offsetWidth - 50 && document.getElementById("opcion").value != "2") {
+            alert("Mala suerte... Ganó el Mercedes");
+            clearInterval(intervalo);
+            break;
+        }
+
+
+        if (coches[2].offsetLeft >= document.getElementById("pista").offsetWidth - 50 && document.getElementById("opcion").value == "3") {
+            alert("Enhorabuena! Ganó el Formula E");
+            clearInterval(intervalo);
+            break;
+        }
+        else if (coches[2].offsetLeft >= document.getElementById("pista").offsetWidth - 50 && document.getElementById("opcion").value != "3") {
+            alert("Mala suerte... Ganó el Formula E");
+            clearInterval(intervalo);
+            break;
+        }
+
+        if (coches[3].offsetLeft >= document.getElementById("pista").offsetWidth - 50 && document.getElementById("opcion").value == "4") {
+            alert("Enhorabuena! Ganó el McLaren");
+            clearInterval(intervalo);
+            break;
+        }
+        else if (coches[3].offsetLeft >= document.getElementById("pista").offsetWidth - 50 && document.getElementById("opcion").value != "4") {
+            alert("Mala suerte... Ganó el McLaren");
+            clearInterval(intervalo);
+            break;
+
+        }
+
     }
 
 
