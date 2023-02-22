97ed49f63660de75ced0815791b300645d26bc44
bieltomas <gtomasvidal@cifpfbmoll.eu>
2023-02-22 03:20:52 +0100

    push 3

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+var intervalo;
+
 function imprimirCoches() {
     document.getElementById("pista").innerHTML = "";
-    document.getElementById("pista").innerHTML += "<img src='images/ferrari.gif' class='coche h-48' id='coche1' />";
-    document.getElementById("pista").innerHTML += "<img src='images/mercedes.gif' class='coche h-48' id='coche2' />";
-    document.getElementById("pista").innerHTML += "<img src='images/formulae.gif' class='coche h-48' id='coche3' />";
+    document.getElementById("pista").innerHTML += "<div class='flex'><img src='images/ferrari.gif' class='coche relative h-48' id='coche1' /></div>";
+    document.getElementById("pista").innerHTML += "<div class='flex'><img src='images/mercedes.gif' class='coche relative h-48' id='coche1' /></div>";
+    document.getElementById("pista").innerHTML += "<div class='flex'><img src='images/formulae.gif' class='coche relative h-48' id='coche1' /></div>";
 }
 
 function carrera() {
@@ -10,16 +12,37 @@ function carrera() {
     for (let i = 0; i < coches.length; i++) {
         if (coches[i].offsetLeft >= document.getElementById("pista").offsetWidth - 50) {
             console.log("Ganó la " + i);
-            break;
+            clearInterval(intervalo);
         } else {
-            let random = Math.random() * 50;
+            let random = Math.floor(Math.random() * 50);
             coches[i].style.left = coches[i].offsetLeft + random + "px";
-            console.log(coches[i].offsetLeft);
+            console.log(coches[i].offsetLeft + random + "px");
+        }
 
+        if (coches[0].offsetLeft >= document.getElementById("pista").offsetWidth - 50) {
+           alert("Ganó el Ferrari");
+            clearInterval(intervalo);
+            break;
+        }
+        else if (coches[1].offsetLeft >= document.getElementById("pista").offsetWidth - 50) {
+            alert("Ganó el Mercedes");
+            clearInterval(intervalo);
+            break;
+        }
+        else if (coches[2].offsetLeft >= document.getElementById("pista").offsetWidth - 50) {
+           alert("Ganó el Formula E");
+            clearInterval(intervalo);
+            break;
         }
     }
 
 
+}
+
+
+function iniciar() {
+    imprimirCoches();
+    intervalo = setInterval(carrera, 100);
 }
 
 
